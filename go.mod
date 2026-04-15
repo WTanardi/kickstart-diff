51b27d2f709e7f15019781c270ddb15e51dc5124
WTanardi <william.tanardi@gmail.com>
2026-04-15 14:25:27 +0700

    feat: prepare for publish

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module kickstart-diff
+module github.com/WTanardi/kickstart-diff
 
 go 1.26.1
 
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "kickstart-diff/cmd"
+import "github.com/WTanardi/kickstart-diff/cmd"
 
 func main() {
 	cmd.Execute()
